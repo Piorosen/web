168293d9743ac67ee6109e5cc23f1563a19dbaa8
Piorosen <aoikazto@gmail.com>
2019-05-13 04:12:05 +0900

    .

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var id = document.getElementById('id')
+var pw = document.getElementById('pw')
+
+function login_click(){
+    alert('a')
+    if (id == 'ak' && pw == 'test'){
+        alert('OH YEEE')
+    }else{
+        alert('fail')
+    }
+
+}
