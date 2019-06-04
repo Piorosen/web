bdd4f1eeb7936fe1ad394016094b29a35b17adaf
Piorosen <aoikazto@gmail.com>
2019-06-05 00:42:06 +0900

    ㅁ

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 
+var canvas;
+var tools;
+
+var x = 0;
+function animate() {
+    let size = canvas.width;
+
+    tools.clearRect(0,0, canvas.width, canvas.height);
+    for (var i = 0; i < imageCount; i++){
+        tools.drawImage(imageList[i], i * 400 - x, 0, 400, 300);
+    }
+    if (x < (imageCount * 400 - size - 600)){
+        x += 1;
+    }else{
+        x = 0;
+    }
+    requestAnimationFrame(animate);
+}
+
+
+var imageCount = 8;
+var imageList = new Array()
+
 
 window.onload = function() {
+    canvas = document.getElementById("moveimage");
+    tools = canvas.getContext("2d");
+
     $(document).ready(function(){
         $("#div1").load("./base/head.html");
         $("#div2").load("./base/foot.html");
     });
+
+    for (var i = 0; i < imageCount; i++){
+        imageList[i] = new Image(400, 300);
+        imageList[i].src = "./main/" + (i + 1) + ".jpg";
+    }
+    animate();
 }
