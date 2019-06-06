57ec52eeb6d18c7405850655418711fab4605aa4
Piorosen <aoikazto@gmail.com>
2019-06-06 12:47:50 +0900

    파싱 툴 시작

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # web
+http://www.horangi.kr/festival.asp?nowyear=2019&nowmonth=6&f_area=&listormap=list
--- parsing/webParsing/webParsing/Program.cs
+++ parsing/webParsing/webParsing/Program.cs
@@ -6,7 +6,7 @@ namespace webParsing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
