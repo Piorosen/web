ebea2685e7ba9974857c22c1f2b59d0a39f2ead0
Piorosen <aoikazto@gmail.com>
2019-06-06 12:30:10 +0900

    파싱 준비

--- list.js
+++ list.js
@@ -1,8 +1,26 @@
 
+function getQueryParam(name) {
+    let url = window.location.search.substring(1);
+    let valueList = url.split('&');
+
+    for (let i = 0; i < valueList.length; i++) {
+        let pair = valueList[i].split('=');
+
+        if (pair[0] == name) {
+            return pair[1];
+        }
+    }
+    return "";
+}
+
 
 window.onload = function() {
     $(document).ready(function(){
         $("#div1").load("./base/head.html");
         $("#div2").load("./base/foot.html");
     });
+
 }
+
+
+
---
+++ parsing/webParsing/webParsing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace webParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
